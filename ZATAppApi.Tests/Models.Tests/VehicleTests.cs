75865627d119da03ac4631427beaa4a9b5f922ac
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2019-08-08 14:44:44 +0500

    Refactorting..

--- ZATAppApi.Tests/Models.Tests/VehicleTests.cs
+++ ZATAppApi.Tests/Models.Tests/VehicleTests.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using ZATApp.Models;
 using Xunit;
-using ZATApp.Models.Exceptions;
 
 namespace ZATAppApi.Tests.Models.Tests
 {
