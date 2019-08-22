7e9aeb51566c76a30e081189c80d8faf668cc976
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2019-08-22 21:43:34 +0500

    Nav bar assembled according to our functionalties

--- ZATAppApi/Controllers/HomeController.cs
+++ ZATAppApi/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ZATApp.Models;
 
 namespace ZATApp.Controllers
 {
@@ -13,6 +14,23 @@ namespace ZATApp.Controllers
             ViewBag.Title = "Home Page";
 
             return View();
+        }
+        /// <summary>
+        /// Method to be called by using AJAX to getting UI updated
+        /// </summary>
+        /// <returns></returns>
+        public int GetUnverifiedTransactionsCount()
+        {
+            int i = 0;
+            try
+            {
+                i = MobileAccountTransactionLog.GetAllUnverifiedMobileAccountTransactions().Count;
+            }
+            catch (Exception)
+            {
+                
+            }
+            return i;
         }
     }
 }
--- ZATAppApi/Models/Common/Constants.cs
+++ ZATAppApi/Models/Common/Constants.cs
@@ -4,5 +4,6 @@
     {
         public static readonly string APPLICATION_NAME = "ZAT";
         public static readonly string TAG_LINE = "Ride Booking App";
+        public static readonly int AJAX_INTERVAL = 1; //min
     }
 }
