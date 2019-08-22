a586be75eaf001beecece842970876c386316aec
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2019-08-22 23:00:08 +0500

    Verify Mobile transactions completed

---
+++ ZATAppApi/Controllers/ErrorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ZATAppApi.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ErrorPage(Exception ex)
+        {
+            return Content(ex.Message);
+        }
+    }
+}
--- ZATAppApi/Controllers/HomeController.cs
+++ ZATAppApi/Controllers/HomeController.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ZATApp.Models;
+using PagedList;
+using PagedList.Mvc;
+using System.ComponentModel.DataAnnotations;
+using ZATApp.ViewModels;
 
 namespace ZATApp.Controllers
 {
+    /// <summary>
+    /// Manages the home tasks for the user
+    /// </summary>
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Controller Starts from this action
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
@@ -28,9 +39,53 @@ namespace ZATApp.Controllers
             }
             catch (Exception)
             {
-                
+
             }
             return i;
+        }
+        /// <summary>
+        /// Method to show a list of unverified transactions in the system
+        /// </summary>
+        /// <param name="page">Page Number us to do paging</param>
+        /// <returns></returns>
+        public ActionResult ViewUnverifiedTransactions(int? page)
+        {
+            try
+            {
+                List<ViewUnverifiedTransactionsViewModel> lstTransactions = new List<ViewUnverifiedTransactionsViewModel>();
+                foreach (var item in MobileAccountTransactionLog.GetAllUnverifiedMobileAccountTransactions())
+                {
+                    lstTransactions.Add(new ViewUnverifiedTransactionsViewModel
+                    {
+                        Id=item.TransactionId,
+                        Amount = item.Amount,
+                        DriverName = item.Driver.FullName.FirstName + " " + item.Driver.FullName.LastName,
+                        IsVerified = item.IsVerified,
+                        ReferenceNumber = item.ReferenceNumber,
+                        ServiceName = item.MobileAccountServiceProviderName,
+                        Time = item.TransactionRegisteredTime.ToString("dd-mmm-yyyy hh:mm:ss")
+                    });
+                }
+                PagedList<ViewUnverifiedTransactionsViewModel> model = new PagedList<ViewUnverifiedTransactionsViewModel>(lstTransactions, page ?? 1, 50);
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("ErrorPage", "Error", ex);
+            }
+        }
+        [HttpGet]
+        public void VerifyTransaction(long id)
+        {
+            try
+            {
+                MobileAccountTransactionLog log = new MobileAccountTransactionLog(id);
+                log.IsVerified = true;
+            }
+            catch (Exception ex)
+            {
+                
+            }
         }
     }
 }
