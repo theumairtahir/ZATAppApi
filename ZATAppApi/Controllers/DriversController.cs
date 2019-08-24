01aa19152353487fc51a638635d6009b2a19ed9a
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2019-08-24 19:44:55 +0500

    Index View Created to show list of drivers

---
+++ ZATAppApi/Controllers/DriversController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZATApp.Models;
+using ZATApp.ViewModels;
+using PagedList;
+using ZATApp.Common;
+
+namespace ZATAppApi.Controllers
+{
+    public class DriversController : Controller
+    {
+        /// <summary>
+        /// Index method of Drivers controller which will return a list of drivers registered with the system
+        /// </summary>
+        /// <param name="page"></param>
+        /// <returns></returns>
+        public ActionResult Index(int? page)
+        {
+            try
+            {
+                List<ViewDriversViewModel> lstDrivers = new List<ViewDriversViewModel>();
+                foreach (var item in Driver.GetAllDrivers())
+                {
+                    var vehicle = item.GetVehicle();
+                    lstDrivers.Add(new ViewDriversViewModel
+                    {
+                        CNIC = item.CNIC_Number,
+                        ContactNumber = item.ContactNumber.LocalFormatedPhoneNumber,
+                        Id = item.UserId,
+                        Name = item.FullName.FirstName + " " + item.FullName.LastName,
+                        VehicleType = vehicle.Type.Name,
+                        IsCleared = item.IsCleared,
+                        Balance = item.Balance
+                    });
+                }
+                PagedList<ViewDriversViewModel> model = new PagedList<ViewDriversViewModel>(lstDrivers, page ?? 1, Constants.PAGGING_RANGE);
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("ErrorPage", "Error", ex);
+            }
+        }
+        public ActionResult ViewDriverDetails(long id)
+        {
+            try
+            {
+                return Content(new Driver(id).CNIC_Number);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("ErrorPage", "Error", ex);
+            }
+        }
+    }
+}
---
+++ ZATAppApi/ViewModels/DriversViewModels.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ZATApp.Common.Validators;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+namespace ZATApp.ViewModels
+{
+    public class ViewDriversViewModel
+    {
+        public long Id { get; set; }
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Display(Name = "Phone Number")]
+        public string ContactNumber { get; set; }
+        [Display(Name = "CNIC Number")]
+        public string CNIC { get; set; }
+        [Display(Name ="Balance")]
+        public decimal Balance { get; set; }
+        [Display(Name = "Vehicle Type")]
+        public string VehicleType { get; set; }
+        [Display(Name ="Cleared")]
+        public bool IsCleared { get; set; }
+    }
+
+}
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
