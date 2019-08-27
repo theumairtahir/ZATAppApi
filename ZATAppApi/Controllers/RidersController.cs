589d37e701cd08a66fcdfc94a3e9adcd367277b8
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2019-08-27 22:33:02 +0500

    Completed Riders Controller

---
+++ ZATAppApi/Controllers/RidersController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZATApp.Models;
+using ZATApp.Common;
+using ZATApp.ViewModels;
+using PagedList;
+
+namespace ZATAppApi.Controllers
+{
+    public class RidersController : Controller
+    {
+        /// <summary>
+        /// Main Action of the riders controller
+        /// </summary>
+        /// <param name="page"></param>
+        /// <returns></returns>
+        public ActionResult Index(int? page)
+        {
+            try
+            {
+                List<ViewRidersViewModel> lstRiders = new List<ViewRidersViewModel>();
+                foreach (var item in Rider.GetAllRiders())
+                {
+                    lstRiders.Add(new ViewRidersViewModel
+                    {
+                        Contact = item.ContactNumber.LocalFormatedPhoneNumber,
+                        Name = item.FullName.FirstName + " " + item.FullName.LastName
+                    });
+                }
+                PagedList<ViewRidersViewModel> model = new PagedList<ViewRidersViewModel>(lstRiders, page ?? 1, Constants.PAGGING_RANGE);
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("ErrorPage", "Error", ex);
+            }
+        }
+        /// <summary>
+        /// Action to return details of a rider
+        /// </summary>
+        /// <param name="id">Primary Key</param>
+        /// <returns></returns>
+        public ActionResult ViewDetails(long id)
+        {
+            try
+            {
+                Rider rider = new Rider(id);
+                var temp = rider.GetCompletedRides();
+                List<Ride> lstTop5CompletedRides;
+                if (temp.Count > 5)
+                {
+                    lstTop5CompletedRides = rider.GetCompletedRides().GetRange(0, 5);
+                }
+                else
+                {
+                    lstTop5CompletedRides = rider.GetCompletedRides().GetRange(0, temp.Count);
+                }
+                ViewRiderDetailsViewModel model = new ViewRiderDetailsViewModel
+                {
+                    CompletedRides = temp.Count,
+                    Contact = rider.ContactNumber.LocalFormatedPhoneNumber,
+                    Name = rider.FullName.FirstName + " " + rider.FullName.LastName,
+                    IsActive = rider.IsActive,
+                    Rides = new List<ViewRiderDetailsViewModel.RideDetailsViewModel>(),
+                    Id = rider.UserId,
+                    IsBlocked = rider.IsBlocked
+                };
+                foreach (var item in lstTop5CompletedRides)
+                {
+                    model.Rides.Add(new ViewRiderDetailsViewModel.RideDetailsViewModel
+                    {
+                        EndPoint = item.Destination,
+                        StartPoint = item.PickUpLocation,
+                        Route = item.Route,
+                        EndTime = item.DropOffTime.ToString("dd-mm-yyyy hh:mm tt"),
+                        StartTime = item.PickUpTime.ToString("dd-mm-yyyy hh:mm tt"),
+                        AmountPaid = decimal.Round(item.GetPaymentSummary().GTotal)
+                    });
+                }
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("ErrorPage", "Error", ex);
+            }
+        }
+        public ActionResult Edit(long id)
+        {
+            ViewBag.ErrorFlag = false;
+            try
+            {
+                Rider rider = new Rider(id);
+                EditRiderViewModel model = new EditRiderViewModel
+                {
+                    CompanyCode = rider.ContactNumber.CompanyCode,
+                    CountryCode = rider.ContactNumber.CountryCode,
+                    FirstName = rider.FullName.FirstName,
+                    Id = rider.UserId,
+                    LastName = rider.FullName.LastName,
+                    Number = rider.ContactNumber.PhoneNumber
+                };
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("ErrorPage", "Error", ex);
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(EditRiderViewModel model)
+        {
+            ViewBag.ErrorFlag = false;
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            try
+            {
+                Rider rider = new Rider(model.Id);
+                rider.FullName = new User.NameFormat
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName
+                };
+                rider.ContactNumber = new User.ContactNumberFormat(model.CountryCode, model.CompanyCode, model.Number);
+                return RedirectToAction("ViewDetails", new { id = model.Id });
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("ErrorPage", "Error", ex);
+            }
+        }
+        /// <summary>
+        /// Action to block a Rider
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Block(long id)
+        {
+            try
+            {
+                Rider rider = new Rider(id);
+                rider.IsBlocked = true;
+                return RedirectToAction("ViewDetails", new { id = id });
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("ErrorPage", "Error", ex);
+            }
+        }
+        /// <summary>
+        /// Action to unblock a rider
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Unblock(long id)
+        {
+            try
+            {
+                Rider rider = new Rider(id);
+                rider.IsBlocked = false;
+                return RedirectToAction("ViewDetails", new { id = id });
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("ErrorPage", "Error", ex);
+            }
+        }
+    }
+}
---
+++ ZATAppApi/ViewModels/RidersViewModels.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using ZATApp.Models;
+using ZATApp.Models.Common;
+
+namespace ZATApp.ViewModels
+{
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    public class ViewRidersViewModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Display(Name = "Phone Number")]
+        public string Contact { get; set; }
+    }
+    public class ViewRiderDetailsViewModel
+    {
+        public long Id { get; set; }
+        [Display(Name = "Full Name")]
+        public string Name { get; set; }
+        [Display(Name = "Phone Number")]
+        public string Contact { get; set; }
+        [Display(Name = "Total Rides Completed")]
+        public int CompletedRides { get; set; }
+        public List<RideDetailsViewModel> Rides { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsBlocked { get; set; }
+
+        public class RideDetailsViewModel
+        {
+            public Location StartPoint { get; set; }
+            public Location EndPoint { get; set; }
+            public Ride.RouteDetails Route { get; set; }
+            public string StartTime { get; set; }
+            public string EndTime { get; set; }
+            public decimal AmountPaid { get; set; }
+        }
+        
+    }
+    public class EditRiderViewModel
+    {
+        public long Id { get; set; }
+        [Display(Name = "First Name")]
+        [Required]
+        public string FirstName { get; set; }
+        [Display(Name = "Second Name")]
+        [Required]
+        public string LastName { get; set; }
+        [Display(Name = "Country Code", Prompt = "+92")]
+        [RegularExpression("[+][1-9][1-9]", ErrorMessage = "Country Code Should be '+92'")]
+        [StringLength(3, ErrorMessage = "Country Code should not be greater or less than 3", MinimumLength = 3)]
+        [Required]
+        public string CountryCode { get; set; }
+        [Display(Name = "Country Code", Prompt = "312")]
+        [StringLength(3, ErrorMessage = "Company Code should not be greater or less than 3", MinimumLength = 3)]
+        [RegularExpression("[3][0-9][0-9]", ErrorMessage = "Company Code must starts from 3 like: 300, 312, 345, 332")]
+        [Required]
+        public string CompanyCode { get; set; }
+        [Display(Name = "Country Code", Prompt = "1234567")]
+        [StringLength(7, ErrorMessage = "Number should not be greater or less than 7", MinimumLength = 7)]
+        [RegularExpression(@"\b\d{7,7}\b", ErrorMessage = "There must be a number like: 1234567")]
+        [Required]
+        public string Number { get; set; }
+        
+    }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+}
